778bcbd59d2747e10320cab69e53c6c70f0d382a
tworiver0931 <tworiver0931@naver.com>
2023-10-24 13:40:37 +0900

    beeep sound

--- src/Scanner copy.js
+++ src/Scanner copy.js
@@ -1,11 +1,27 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
+import { gql, useMutation } from "@apollo/client";
 import { Html5QrcodeScanner } from "html5-qrcode";
 import beep from "./beep.mp3";
+
+// const USETICKET_MUTATION = gql`
+//   mutation useTicket($userId: String!, $fundId: String!) {
+//     useTicket(userId: $userId, fundId: $fundId) {
+//       ok
+//       error
+//     }
+//   }
+// `;
 
 function Scanner() {
   const [scanResult, setScanResult] = useState(null);
   const [showScanSuccess, setShowScanSuccess] = useState(false);
-  const scannerRef = useRef(null);
+  const [refresh, setRefresh] = useState(null);
+
+  // const onCompleted = (data) => {};
+
+  // const [useTicket, { loading }] = useMutation(USETICKET_MUTATION, {
+  //   onCompleted,
+  // });
 
   useEffect(() => {
     const scanner = new Html5QrcodeScanner("reader", {
@@ -16,33 +32,27 @@ function Scanner() {
       fps: 5,
       verbose: false,
     });
-
-    scannerRef.current = scanner;
-
     scanner.render(success, error);
     var audio = new Audio(beep);
 
     function success(result) {
       audio.play();
+      scanner.clear();
       // result: {fundId, left}
       setScanResult(JSON.parse(result));
+      console.log(result);
       setShowScanSuccess(true);
 
       setTimeout(() => {
         setShowScanSuccess(false);
+        setRefresh((prev) => (prev == 1 ? 0 : 1));
       }, 1500);
     }
 
     function error(err) {
       console.warn(err);
     }
-
-    return () => {
-      if (scannerRef.current) {
-        scannerRef.current.clear();
-      }
-    };
-  }, []);
+  }, [refresh]);
 
   return (
     <div
@@ -54,16 +64,7 @@ function Scanner() {
       }}
     >
       <h1>DBUS QR</h1>
-      <div
-        id="reader"
-        style={{
-          width: "500px",
-          height: "500px",
-          overflow: "hidden",
-          borderRadius: "15px",
-        }}
-      />
-      {showScanSuccess && (
+      {showScanSuccess ? (
         <div
           style={{
             width: "500px",
@@ -74,8 +75,6 @@ function Scanner() {
             flexDirection: "column",
             alignItems: "center",
             justifyContent: "center",
-            position: "absolute",
-            backgroundColor: "rgba(255, 255, 255, 0.8)",
           }}
         >
           <a style={{ fontWeight: "bold", fontSize: "16px" }}>
@@ -86,6 +85,17 @@ function Scanner() {
             남은 티켓: {scanResult.left}
           </a>
         </div>
+      ) : (
+        <div
+          id="reader"
+          style={{
+            width: "500px",
+            height: "500px",
+            border: "medium solid black",
+            overflow: "hidden",
+            borderRadius: "15px",
+          }}
+        />
       )}
     </div>
   );
